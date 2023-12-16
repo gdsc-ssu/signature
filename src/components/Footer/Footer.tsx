66a29723f41437f00d51dcfabe410c1e084a075c
fecapark <fecapark@gmail.com>
2023-12-16 19:54:23 +0900

    design: Footer 레이아웃에 디자인 적용 완료

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,33 @@
+import styles from './Footer.module.css'
+
+interface INameLinkProps {
+  href: string
+  children: React.ReactNode
+}
+
+const NameLink = ({ href, children }: INameLinkProps) => {
+  return (
+    <a className={styles['name-link']} href={href} target="_blank" rel="noopener noreferrer">
+      {children}
+    </a>
+  )
+}
+
+const Dot = () => {
+  return <div className={styles.dot}></div>
+}
+
 export const Footer = () => {
   return (
-    <footer>
-      <span>Made by itjustbong · Hanna922 · fecapark</span>
+    <footer className={styles.footer}>
+      <div className={styles.madeby}>
+        <span>Made by</span>
+        <NameLink href="https://github.com/itjustbong">itjustbong</NameLink>
+        <Dot />
+        <NameLink href="https://github.com/Hanna922">Hanna922</NameLink>
+        <Dot />
+        <NameLink href="https://github.com/fecapark">fecapark</NameLink>
+      </div>
     </footer>
   )
 }
