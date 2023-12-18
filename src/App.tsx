066185cdd3bcaf0ae97b0eb307afabb351f2d26a
Hanna922 <hanna.urssu@gmail.com>
2023-12-18 23:57:30 +0900

    refactor: App.tsx에서 Pixi 로직 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,102 +1,14 @@
-import * as PIXI from 'pixi.js'
+import { useEffect } from 'react'
 
 import { Footer } from '@/components/Footer/Footer'
 import { Header } from '@/components/Header/Header'
-import { Visual } from '@/components/Visual/Visual'
+import { PixiManager } from '@/components/PixiManager/PixiManager'
 
 export const App = () => {
-  let renderer!: PIXI.Renderer
-  let stage!: PIXI.Container
-  const { show, animate } = Visual()
-
-  let stageWidth!: number
-  let stageHeight!: number
-
-  const initRenderer = () => {
-    renderer = new PIXI.Renderer({
-      width: document.body.clientWidth,
-      height: document.body.clientHeight,
-      antialias: true,
-      transparent: false,
-      resolution: window.devicePixelRatio > 1 ? 2 : 1,
-      autoDensity: true,
-      powerPreference: 'high-performance',
-      backgroundColor: 0xffffff,
-    })
-  }
-
-  const renderRenderer = () => {
-    document.body.appendChild(renderer.view)
-  }
-
-  const initStage = () => {
-    stage = new PIXI.Container()
-  }
-
-  const addFilterToStage = () => {
-    const blurFilter = new PIXI.filters.BlurFilter()
-    blurFilter.blur = 10
-    blurFilter.autoFit = true
-
-    const fragSource = `
-      precision mediump float;
-      varying vec2 vTextureCoord;
-      uniform sampler2D uSampler;
-      uniform float threshold;
-      uniform float mr;
-      uniform float mg;
-      uniform float mb;
-      void main(void) {
-        vec4 color = texture2D(uSampler, vTextureCoord);
-        vec3 mcolor = vec3(mr, mg, mb);
-        if(color.a > threshold) {
-          gl_FragColor = vec4(mcolor, 1.0);
-        } else {
-          gl_FragColor = vec4(vec3(0.0), 0.0);
-        }
-      }
-    `
-
-    const uniformsData = {
-      threshold: 0.5,
-      mr: 180.0 / 255.0,
-      mg: 100.0 / 255.0,
-      mb: 200.0 / 255.0,
-    }
-
-    const thresholdFilter = new PIXI.Filter(undefined, fragSource, uniformsData)
-    stage.filters = [blurFilter, thresholdFilter]
-
-    stage.filterArea = renderer.screen
-  }
-
-  const resize = () => {
-    stageWidth = document.body.clientWidth
-    stageHeight = document.body.clientHeight
-
-    renderer.resize(stageWidth, stageHeight)
-
-    show(stageWidth, stageHeight, stage)
-  }
-
-  const appAnimate = () => {
-    requestAnimationFrame(appAnimate)
-
-    animate()
-
-    renderer.render(stage)
-  }
-
-  initRenderer()
-  renderRenderer()
-
-  initStage()
-  addFilterToStage()
-
-  window.addEventListener('resize', resize.bind(this), false)
-  resize()
-
-  requestAnimationFrame(appAnimate.bind(this))
+  useEffect(() => {
+    const pixiManager = new PixiManager()
+    pixiManager.animate()
+  }, [])
 
   return (
     <>
---
+++ src/components/PixiManager/PixiManager.ts
@@ -0,0 +1,84 @@
+import * as PIXI from 'pixi.js'
+
+import { Visual } from '@/components/Visual/Visual'
+
+export class PixiManager {
+  private renderer!: PIXI.Renderer
+  private stage!: PIXI.Container
+  private visual: ReturnType<typeof Visual>
+
+  private readonly colorRed = 180.0 / 255.0
+  private readonly colorGreen = 100.0 / 255.0
+  private readonly colorBlue = 200.0 / 255.0
+
+  constructor() {
+    this.initRenderer()
+    this.initStage()
+    this.visual = Visual()
+    this.addFilterToStage()
+    window.addEventListener('resize', this.resize.bind(this), false)
+    this.resize()
+  }
+
+  private initRenderer(): void {
+    this.renderer = new PIXI.Renderer({
+      width: document.body.clientWidth,
+      height: document.body.clientHeight,
+      antialias: true,
+      transparent: false,
+      resolution: window.devicePixelRatio > 1 ? 2 : 1,
+      autoDensity: true,
+      powerPreference: 'high-performance',
+      backgroundColor: 0xffffff,
+    })
+    document.body.appendChild(this.renderer.view)
+  }
+
+  private initStage(): void {
+    this.stage = new PIXI.Container()
+  }
+
+  private addFilterToStage(): void {
+    const blurFilter = new PIXI.filters.BlurFilter()
+    blurFilter.blur = 10
+    blurFilter.autoFit = true
+
+    const fragSource = `
+      precision mediump float;
+      varying vec2 vTextureCoord;
+      uniform sampler2D uSampler;
+      uniform float threshold;
+      uniform vec3 mcolor;
+      void main(void) {
+        vec4 color = texture2D(uSampler, vTextureCoord);
+        if (color.a > threshold) {
+          gl_FragColor = vec4(mcolor, 1.0);
+        } else {
+          gl_FragColor = vec4(vec3(0.0), 0.0);
+        }
+      }
+    `
+
+    const uniformsData = {
+      threshold: 0.5,
+      mcolor: [this.colorRed, this.colorGreen, this.colorBlue],
+    }
+
+    const thresholdFilter = new PIXI.Filter(undefined, fragSource, uniformsData)
+    this.stage.filters = [blurFilter, thresholdFilter]
+    this.stage.filterArea = this.renderer.screen
+  }
+
+  private resize(): void {
+    const stageWidth = document.body.clientWidth
+    const stageHeight = document.body.clientHeight
+    this.renderer.resize(stageWidth, stageHeight)
+    this.visual.show(stageWidth, stageHeight, this.stage)
+  }
+
+  public animate(): void {
+    requestAnimationFrame(this.animate.bind(this))
+    this.visual.animate()
+    this.renderer.render(this.stage)
+  }
+}
