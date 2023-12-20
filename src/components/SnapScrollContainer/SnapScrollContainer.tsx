8a605e25303e417e8d75d3d426abfb35d18f87a1
Sanghyeok Park <fecapark@gmail.com>
2023-12-20 14:20:13 +0900

    Update SnapScrollContainer.tsx
    인터페이스 표기법 앞에 I 제거

--- src/components/SnapScrollContainer/SnapScrollContainer.tsx
+++ src/components/SnapScrollContainer/SnapScrollContainer.tsx
@@ -2,19 +2,19 @@ import React from 'react'
 
 import styles from './SnapScrollContainer.module.css'
 
-interface ISnapScrollContainerProps {
+interface SnapScrollContainerProps {
   items: Array<React.ReactNode>
 }
 
-interface ISnapScrollItemProps {
+interface SnapScrollItemProps {
   children: React.ReactNode
 }
 
-const SnapScrollItem = ({ children }: ISnapScrollItemProps) => {
+const SnapScrollItem = ({ children }: SnapScrollItemProps) => {
   return <div className={styles.item}>{children}</div>
 }
 
-export const SnapScrollContainer = ({ items }: ISnapScrollContainerProps) => {
+export const SnapScrollContainer = ({ items }: SnapScrollContainerProps) => {
   return (
     <div className={styles.container}>
       {items.map((item, index) => (
