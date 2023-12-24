fdc6cd8955beefb8e9f94e220115da1903a5909b
Hanna922 <hanna.urssu@gmail.com>
2023-12-24 15:26:34 +0900

    feat: add IntersectionObserver in SnapScrollItem1

--- src/components/SnapScrollItem/SnapScrollItem1.tsx
+++ src/components/SnapScrollItem/SnapScrollItem1.tsx
@@ -1,15 +1,39 @@
 import { useEffect, useRef } from 'react'
 
-import { init } from '@/components/TextDrawing/TextDrawing'
+import { Init, TextDrawing } from '@/components/TextDrawing/TextDrawing'
 
 export const SnapScrollItem1 = ({ id }: { id: string }) => {
+  const containerRef = useRef<HTMLDivElement>(null)
   const isRender = useRef<boolean>(false)
 
   useEffect(() => {
-    if (isRender.current) return
-    isRender.current = true
-    init(id)
-  }, [])
+    const observerCallback: IntersectionObserverCallback = (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          // Check if this is the first render
+          if (!isRender.current) {
+            isRender.current = true
+            Init(id)
+          } else {
+            // Re-trigger the animation
+            TextDrawing()
+          }
+        }
+      })
+    }
 
-  return <div style={{ height: '100%' }} id={id} />
+    // Setting up the Intersection Observer
+    const observer = new IntersectionObserver(observerCallback, {
+      threshold: 0.1,
+    })
+
+    // Observing the current element
+    if (containerRef.current) {
+      observer.observe(containerRef.current)
+    }
+
+    return () => observer.disconnect()
+  }, [id])
+
+  return <div ref={containerRef} style={{ height: '100%' }} id={id} />
 }
